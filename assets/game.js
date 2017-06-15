87e8001092b2fd5cb9220670a2cd4a1b30c54840
Willistan <wiriamutan@gmail.com>
2017-06-15 03:50:30 -0700

    Add files via upload

---
+++ assets/game.js
@@ -0,0 +1,24 @@
+(function(){
+
+	window.onload = function() {
+		isSupported();
+		grid();
+	};	
+
+	function isSupported(){
+		// Check if rot.js can work on this browser
+		if (!ROT.isSupported()) {
+		    alert("The rot.js library isn't supported by your browser.");
+		} else {
+		    // Good to go!
+		}
+	}
+
+	function grid(){
+		// Create a display 80 characters wide and 20 characters tall
+		var display = new ROT.Display({width:80, height:20});
+		var container = display.getContainer();
+		// Add the container to our HTML page
+		document.body.appendChild(container);
+	}
+})();
